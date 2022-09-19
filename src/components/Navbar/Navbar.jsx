2e78a24092226b04e1c2298c625891112a4b427a
Elroli09 <yoldi.gustavo@gmail.com>
2022-09-18 23:34:20 -0300

    Added Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ochentaNoventaImg from "../../assets/images/ochentaNoventaImg.jpg";
+import Styles from "./Navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <div className={Styles.container}>
+      <h1>OCHENTA<br/>NOVENTA</h1>
+    </div>
+  );
+}
+
+{/* <img src={ochentaNoventaImg} alt="ochentaNoventaImg" /> */}
