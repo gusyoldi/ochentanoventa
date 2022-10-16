a6c1e8da80de2e677b3807e96fbac1f7ca4fbe4e
Elroli09 <yoldi.gustavo@gmail.com>
2022-10-16 14:12:25 -0300

    Installed react-hook-form to perform validations

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,52 +1,53 @@
 // import React from 'react'
 import Styles from "./ContactUs.module.css";
-/* EmailJS */
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-/* EmailJS */
+import { useForm } from "react-hook-form";
 
 export default function ContactUs() {
   const form = useRef();
 
-  // const sendEmail = (e) => {
-  //   e.preventDefault()
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  // const onSubmit = data => console.log(data);
+ 
 
-  //   emailjs.sendForm('service_mtdmr5r', 'template_ntmq6rx', form.current, 'm4-E800NJXGTBTwxH')
-  //   .then((result) => {
-  //       console.log(result.text);
-  //   }, (error) => {
-  //       console.log(error.text);
-  //   alert("Mensaje enviado!")
-  //   })
-  //   e.preventDefault()
-  //   alert("TU MENSAJE FUE ENVIADO!!")
-  // }
+  const sendEmail = () => {
+    // e.preventDefault()
+
+    emailjs.sendForm('service_mtdmr5r', 'template_ntmq6rx', form.current, 'm4-E800NJXGTBTwxH')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    alert("Mensaje enviado!")
+    })
+  }
 
   return (
     <div className={Styles.container}>
       <h4>Pedí tu presupuesto</h4>
-      <form ref={form} /*  onSubmit={sendEmail} */>
+      <form ref={form} onSubmit={handleSubmit(sendEmail)} /* onSubmit={sendEmail} */>
         <label>
           <h6>
-            Nombre Completo <span>*</span>
+            Nombre Completo <span>*</span> {errors.user_name && <span>Es necesario un nombre</span>}
           </h6>
         </label>
-        <input type="text" id="user_name" name="user_name" />
+        <input type="text" id="user_name" name="user_name" {...register("user_name", {required: true})} />
         <label>
           <h6>
-            Email <span>*</span>
+            Email <span>*</span> {errors.user_email && <span>Es necesario un email</span>}
           </h6>
         </label>
-        <input type="text" id="user_email" name="user_email" />
+        <input type="email" id="user_email" name="user_email" {...register("user_email", {required: true})}  />
         <label>
           <h6>
-            Teléfono <span>*</span>
+            Teléfono <span>*</span> {errors.user_phone && <span>Es necesario un número</span>}
           </h6>
         </label>
-        <input type="number" id="phone" name="phone" />
+        <input type="number" id="phone" name="phone" {...register("user_phone", {required: true})}  />
         <label>
           <h6>
-            Consulta <span>*</span>
+            Consulta <span>*</span> {errors.message && <span>Es necesario un mensaje</span>}
           </h6>
         </label>
         <textarea
@@ -54,8 +55,10 @@ export default function ContactUs() {
           rows="10"
           cols="30"
           placeholder="Contanos un poco mas de tu evento..."
+          {...register("message", {required: true})}
         ></textarea>
-        <button type="submit" value="Send">
+        
+        <button type="submit" value="Send" >
           ENVIAR CONSULTA
         </button>
       </form>
