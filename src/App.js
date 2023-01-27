811393b696e7253279d15518d706663f5569fa19
Elroli09 <yoldi.gustavo@gmail.com>
2023-01-27 15:21:01 -0300

    Change Youtube for Albumns

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     <div className="App">
       <Header />
       <About />
-      <Albums />
       <YoutubeVideo />
+      <Albums />
       <ContactUs />
       <FollowUs />
     </div>
