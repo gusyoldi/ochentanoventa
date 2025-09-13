aef514c44e87ad21b6281e4966c3caa8d385f1a0
Gus <yoldi.gustavo@gmail.com>
2025-09-13 11:35:26 -0300

    add new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,13 +28,14 @@ export default function HomePage() {
             <span className="font-bold">Rock Argentino</span> para hacer sus
             propias versiones, combinando estilos como{' '}
             <span className="font-bold">
-              Country, Folk, Rock & Roll y Blues.
+              Country, Folk, Rock & Roll y Blues
             </span>
+            .
           </p>
           <p>
             Es un <span className="font-bold">proyecto artístico</span> con un
             formato ideal para sonar en{' '}
-            <span className="font-bold">eventos.</span>
+            <span className="font-bold">eventos</span>.
           </p>
           <p>
             Instrumentos acústicos como el Banjo, la Mandolina, el Washboard y
@@ -86,6 +87,41 @@ export default function HomePage() {
           />
           <span>Escuchar ahora</span>
         </a>
+      </section>
+      <section className="flex flex-col items-center py-10 xl:grid xl:grid-flow-col xl:grid-rows-4 xl:items-start xl:gap-x-3.5 xl:py-20">
+        <div className="font-montserrat text-center leading-8 uppercase xl:col-span-1 xl:row-start-2 xl:text-start xl:leading-14">
+          <h2 className="text-heading-md xl:text-heading-xl font-extrabold text-nowrap">
+            Eventos
+          </h2>
+          <h3 className="text-gold text-heading-sm xl:text-heading-lg mb-5 font-bold xl:mb-10">
+            Privados
+          </h3>
+        </div>
+        <div className="mb-5 h-[350px] w-[350px] bg-stone-400 xl:order-first xl:row-span-6 xl:h-[590px] xl:w-[590px]">
+          {/* <ImageCarousel images={images} /> */}
+        </div>
+        <div className="text-body-sm font-roboto xl:text-body-md flex max-w-96 flex-col gap-6 leading-5 tracking-wide xl:col-span-1 xl:row-span-2">
+          <p>
+            Con más de 40 shows en vivo,{' '}
+            <span className="font-bold">Ochenta Noventa</span> continúa
+            programando recepciones de{' '}
+            <span className="font-bold">Casamientos</span>,{' '}
+            <span className="font-bold">Cumpleaños</span> y eventos{' '}
+            <span className="font-bold">corporativos</span>.
+          </p>
+          <p>
+            <span className="font-bold">Formatos:</span>
+            <br />
+            <span className="font-bold">Acústico</span> para disfrutar de los
+            sonidos de instrumentos característicos del Country.
+            <br />
+            <span className="font-bold">Eléctrico</span> para festejar bien
+            arriba.
+            <br />
+            <span className="font-bold">Mixto</span> para un evento súper
+            climático y completo.
+          </p>
+        </div>
       </section>
     </main>
   );
