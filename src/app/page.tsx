5970672395ebe852b246ae6724c4f2886e7e8010
Gus <yoldi.gustavo@gmail.com>
2025-11-17 00:03:27 -0300

    refactor: contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Banner from '@/lib/components/Banner';
-import ImageCarousel from '@/lib/components/Carousel';
+import ContactSection from '@/lib/components/ContactSection';
 import EventsSection from '@/lib/components/EventsSection';
-import Form from '@/lib/components/Form';
 import HeroSection from '@/lib/components/HeroSection';
 import VersionsSection from '@/lib/components/VersionsSection';
 
@@ -23,14 +22,7 @@ export default async function HomePage() {
 
       <EventsSection carouselImages={carousel.second} />
 
-      <section className="pb-10 xl:pb-20">
-        <div className="bg-secondary mx-auto mb-10 rounded-md sm:hidden xl:mb-20 xl:max-w-[874px]">
-          <Form />
-        </div>
-        <div className="mx-auto h-[350px] w-[350px] xl:order-first xl:row-span-6 xl:h-[800px] xl:w-[1280px]">
-          <ImageCarousel images={carousel.third} size="lg" />
-        </div>
-      </section>
+      <ContactSection carouselImages={carousel.third} />
     </main>
   );
 }
--- src/lib/components/EventsSection.tsx
+++ src/lib/components/EventsSection.tsx
@@ -12,17 +12,14 @@ export default function EventsSection({ carouselImages }: EventsSectionProps) {
 
   return (
     <section className="flex flex-col items-center py-10 xl:grid xl:grid-flow-col xl:grid-rows-4 xl:items-start xl:gap-x-3.5 xl:py-20">
-      {/* Title Section */}
       <div className="xl:col-span-1 xl:row-start-2">
         <TitleSection title={title} subtitle={subtitle} />
       </div>
 
-      {/* Carousel */}
       <div className="mb-5 h-[350px] w-[350px] xl:order-first xl:row-span-6 xl:h-[590px] xl:w-[590px]">
         <ImageCarousel images={carouselImages} />
       </div>
 
-      {/* Description */}
       <EventsDescription />
     </section>
   );
