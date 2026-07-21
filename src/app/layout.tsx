ee77e56f70182252c90b72834cae6b73a189f405
Gus <yoldi.gustavo@gmail.com>
2026-07-20 23:53:00 -0300

    add mixpanel config to analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,15 @@ import Toaster from '@/components/ui/Toaster';
 import { layoutMetadata } from './_lib/seo/metadata';
 import './_styles/globals.css';
 
+//Import Mixpanel SDK
+import mixpanel from "mixpanel-browser";
+
+// Create an instance of the Mixpanel object, your token is already added to this snippet
+mixpanel.init('47a7a696fc445fed7c658478583857bf', {
+  autocapture: true,
+  record_sessions_percent: 100,
+})
+
 export const metadata = layoutMetadata;
 
 export default function RootLayout({
